104cb5f947f370f182a8215342b3685bb96e4e3a
patrycja <patrycja0figat@gmail.com>
2023-03-04 13:49:48 +0100

    [change] - anonymous classes examples

--- src/main/java/pl/sda/orange2/lambda/LambdaExperiences.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExperiences.java
@@ -8,5 +8,15 @@ public class LambdaExperiences {
 
       ChlopakiNiePlacza boy2 = new ChlopakiNiePlacza();
       boy2.sing();
+
+
+      //klasa anonimowa
+      BoysDontCry anonymousClassReference = new BoysDontCry() {
+          @Override
+          public void silnoreki() {
+              System.out.println("Anonimowy Silnoreki");
+          }
+      };
+      anonymousClassReference.silnoreki();
     }
 }
