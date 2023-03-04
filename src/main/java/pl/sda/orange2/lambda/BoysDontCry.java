eca2afc55b6f3ec33f79d4113184cd5dc5ab063e
patrycja <patrycja0figat@gmail.com>
2023-03-04 14:40:15 +0100

    [change] - using @FunctionalInterface and describing its purpose 1). @FunctionalInterface annotation 2). Default methods in interfaces

--- src/main/java/pl/sda/orange2/lambda/BoysDontCry.java
+++ src/main/java/pl/sda/orange2/lambda/BoysDontCry.java
@@ -1,6 +1,8 @@
 package pl.sda.orange2.lambda;
 
 //its functional interface because has one abstract method
+@FunctionalInterface //ochrona przed błędami ludzkimi
 public interface BoysDontCry {
     void silnoreki();
+    default void cry() {};
 }
