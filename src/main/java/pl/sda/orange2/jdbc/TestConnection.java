ac4f4a2e46ca905556210559c9181da35b79611c
patrycja <patrycja0figat@gmail.com>
2023-03-05 15:27:18 +0100

    [change]

---
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -0,0 +1,20 @@
+package pl.sda.orange2.jdbc;
+
+import pl.sda.orange2.jdbc.config.H2Config;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+    public static void main(String[] args) {
+        try {
+            var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+                    H2Config.USER,
+                    H2Config.PASSWORD);
+            System.out.println("got connection: " + (h2Connection != null));
+        } catch (SQLException exc) {
+            System.out.println("got exception: " + exc);
+        }
+    }
+}
+
