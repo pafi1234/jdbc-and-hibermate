4b9f799147aa60607df47eba4448c73ded4d0026
patrycja <patrycja0figat@gmail.com>
2023-03-05 16:06:04 +0100

    [change] - test query example

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -2,19 +2,44 @@ package pl.sda.orange2.jdbc;
 
 import pl.sda.orange2.jdbc.config.H2Config;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TestConnection {
     public static void main(String[] args) {
+        String query = """
+                SELECT ID, NAME
+                FROM TEST
+                """;
+
         try {
+//            Connection h2Connection = DriverManager.getConnection(H2Config.DB_URL,
+//                    H2Config.USER,
+//                    H2Config.PASSWORD);
+            // we need jdbc driver (h2 dependency at pom)
+            // connection gave us access to existing db
             var h2Connection = DriverManager.getConnection(H2Config.DB_URL,
                     H2Config.USER,
                     H2Config.PASSWORD);
             System.out.println("got connection: " + (h2Connection != null));
+
+            // Statement is used to send queries to db with existing connection
+            Statement queryStatement = h2Connection.createStatement();
+
+            // ResultSet contains query result data as simple table
+            // we need to iterate over result to got data
+            ResultSet queryResult = queryStatement.executeQuery(query);
+            while (queryResult.next()) {
+                System.out.println("id: " + queryResult.getInt(1));
+                System.out.println("name: " + queryResult.getString(2));
+            }
         } catch (SQLException exc) {
             System.out.println("got exception: " + exc);
+            exc.printStackTrace();
         }
     }
 }
+
+
+
+
 
