de444ffc3b325c1236b167ee1fd9afda05238ecd
patrycja <patrycja0figat@gmail.com>
2023-03-07 19:13:36 +0100

    [change] - test query example

---
+++ src/main/java/pl/sda/orange2/enumarable/DbConfig.java
@@ -0,0 +1,16 @@
+package pl.sda.orange2.enumerable;
+
+public enum DbConfig {
+    DB_URL("jdbc:h2:~/databases/jdbc"),
+    USER("sa"),
+    PASSWORD("sa");
+
+    private String value;
+
+    DbConfig(String value) {
+        this.value = value;
+    }
+    public String getValue() {
+        return value;
+    }
+}
